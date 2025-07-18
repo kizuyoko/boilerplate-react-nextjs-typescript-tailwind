c3e52ffff2e2e533d8ce6622f54cd9df149a3eb0
kizuyoko <kizuyoko@gmail.com>
2025-07-18 15:31:44 +0200

    Basic texts and styles

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ This is very easy and simple starter set of React.js, Next.js, TypeScript, and T
 ## Demo
 Deployed to Vercel, [here](https://boilerplate-react-nextjs-typescript.vercel.app)
 
-## TailwindCSS setting
-globals.css
+## To Customise
+- Favicon: src/app/favicon.ico
+- TailwindCSS setting: src/globals.css
+- Font: app/layout.tsx, tailwind.config.js, tailwind.config.js
+- Meta data (page title, description, etc): Default is on src/app/layout.tsx and you can add to each pages.
+
 
 ## How to start React Next.js TypeScript TailwindCSS manually from scratch
 Use this code in terminal:
@@ -48,19 +52,3 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <header>
-        Home | About
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-20 sm:p-20">
+      <header className="flex items-center justify-center gap-4">
+        <a className="text-link">Home</a> | 
+        <a className="text-link">About</a>
       </header>
-      <main className="flex flex-col gap-10 items-center justify-center">
+      <main className="flex flex-col gap-4 items-center justify-center">
         <h1>Boilerplate React Next.js TypeScript TailwindCSS</h1>
         <p>This is very easy and simple starter set of React.js, Next.js, TypeScript, and TailwindCSS.</p>
         <p>Feel free to copy and use it!</p>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="button"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -19,51 +20,35 @@ export default function Home() {
             About
           </a>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-4 flex-wrap items-center justify-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="image-link"
+          href="https://github.com/kizuyoko/boilerplate-react-nextjs-typescript-tailwind/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
+            src="/github.svg"
+            alt="GitHub"
+            width={30}
+            height={30}
+          /> 
+         
+        </a> 
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="image-link"
+          href="https://www.linkedin.com/in/kizuyoko/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+            src="/linkedin.svg"
+            alt="LinkedIn"
+            width={30}
+            height={30}
+          /> 
         </a>
       </footer>
     </div>
